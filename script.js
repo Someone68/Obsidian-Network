ad3259867b2c6d77f1a33b897f52e4c8e84888df
Someone68 <68.someone.m@gmail.com>
2023-09-27 14:49:49 -0700

    settings page, cloakig

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ function everySecond() {
 
 setInterval(everySecond, 1000);
 everySecond();
+
+function checkCon() {
+	if (localStorage.favicon) {
+		if (localStorage.title) {
+			if (
+				localStorage.title.trim().length >= 1 &&
+				localStorage.favicon.trim().length >= 1
+			) {
+				s("title").innerHTML = localStorage.title;
+				s(".favicon").href = localStorage.favicon;
+			}
+		}
+	}
+}
+
+checkCon();
+window.onload = checkCon;
+
+function disguise(img, text) {
+	localStorage.favicon = img;
+	localStorage.title = text;
+	checkCon();
+}
+
+function customdis() {
+	let text = prompt("Tab Name*: ");
+	let favicon = prompt("Tab Icon Link (OPTIONAL): ");
+	if (!favicon) {
+		favicon = "not set will cause 404 but it fine";
+	}
+	localStorage.favicon = favicon;
+	localStorage.title = text;
+	checkCon();
+	alert("Disguise Set!");
+}
+
+function cloak() {
+	let tab = window.open("about:blank");
+	tab.document.body.innerHTML = `<style>*{padding:0;margin:0;}html,body{width:100%;height:100vh}</style><iframe src="https://lunar-eclipse.pages.dev" style="border:0px #ffffff none;" name="lunar" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="100%" width="100%" allowfullscreen></iframe>`;
+}
