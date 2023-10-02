49524dedc2af6f3b48201384a5c2ad30287e1397
Someone68 <68.someone.m@gmail.com>
2023-10-01 20:06:32 -0700

    qol bugfix

--- settings.js
+++ settings.js
@@ -48,9 +48,19 @@ document.onclick = () => {
 	}
 	if (!localStorage.flowlink && document.activeElement === document.body) {
 		localStorage.flowlink = "https://mymaths.academy/";
+		s("#flowos").value = localStorage.flowlink;
+	} else if (
+		localStorage.flowlink &&
+		document.activeElement === document.body
+	) {
+		if (localStorage.flowlink.toLowerCase() === "https://")
+			localStorage.flowlink = "https://mymaths.academy/";
+
+		s("#flowos").value = localStorage.flowlink;
+	} else {
+		localStorage.prxylink = s("#prxyl").value;
+		s("#flowos").value = localStorage.flowlink;
 	}
-	localStorage.prxylink = s("#prxyl").value;
-	s("#flowos").value = localStorage.flowlink;
 	checkCon();
 
 	if (!localStorage.prxylink.includes("https://")) {
@@ -60,9 +70,19 @@ document.onclick = () => {
 	}
 	if (!localStorage.prxylink && document.activeElement === document.body) {
 		localStorage.prxylink = "https://leclipse.onrender.com/";
+		s("#prxyl").value = localStorage.prxylink;
+	} else if (
+		localStorage.prxylink &&
+		document.activeElement === document.body
+	) {
+		if (localStorage.prxylink.toLowerCase() === "https://")
+			localStorage.prxylink = "https://leclipse.onrender.com/";
+
+		s("#prxyl").value = localStorage.prxylink;
+	} else {
+		localStorage.panicurl = s("#panicurl").value;
+		s("#prxyl").value = localStorage.prxylink;
 	}
-	localStorage.panicurl = s("#panicurl").value;
-	s("#prxyl").value = localStorage.prxylink;
 	checkCon();
 	if (!localStorage.panicurl.includes("https://")) {
 		let newlcs = localStorage.panicurl.split("");
@@ -71,8 +91,17 @@ document.onclick = () => {
 	}
 	if (!localStorage.panicurl && document.activeElement === document.body) {
 		localStorage.panicurl = "https://google.com/";
+		s("#panicurl").value = localStorage.panicurl;
+	} else if (
+		localStorage.panicurl &&
+		document.activeElement === document.body
+	) {
+		if (localStorage.panicurl.toLowerCase() === "https://")
+			localStorage.panicurl = "https://google.com/";
+		s("#panicurl").value = localStorage.panicurl;
+	} else {
+		localStorage.panicurl = s("#panicurl").value;
+		s("#panicurl").value = localStorage.panicurl;
 	}
-	localStorage.panicurl = s("#panicurl").value;
-	s("#panicurl").value = localStorage.panicurl;
 	checkCon();
 };
